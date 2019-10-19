acdb610af352be8e26dd7788afce69d02bf2958b
PacificPR <prashant.kumarjha63@gmail.com>
2019-10-20 00:19:47 +0530

    SWITCH.c FIXED

--- switch/switch.c
+++ switch/switch.c
@@ -1,39 +1,44 @@
-#include<conio.h>
-#include <stdio.h>
+#include<curses.h>
+#include <stdlib.h>
 #include<stdio.h>
+//including headers, curses.h instead of conio.h as it is supported by latest
+//unix machines
 
+int incr(int x);
+int decr(int x);
 
 int main()
 {
     int num,ch;
     printf("enter the  two digit number of your choice");
     scanf("%d",&num);
-    while(ch!=2){
+    //accepting the number 
+    
+    int temp1,res;
+    while(true){
     printf("This is the following menu options\n");
     printf("----------1.Choose here for incrementing the number-----------------\n");
     printf("----------2.Choose here for decrementing the number-------------------\n");
     printf("----------3.EXIT THE PROGRAM----------------------------\n");
     printf("enter the choice");
     scanf("%d",&ch);
+    //accepting choice of user
     
-    switch(num) {
-        case 1: {
-            int res;
-            res=incr(num);
-            printf("the incremented number is %d\n",res);
-            break;
-        }
-        case 2:{
-            int res;
-            res=decr(num);
-            printf("the decremented number is %d\n",res);
-            break;
-            
-        }
-        case 3:{
-            exit(0);
-            break;
-        }
+    switch(ch)
+    {
+        case 1: res=incr(num);
+                printf("the incremented number is %d\n",res);
+                num=res;
+                break;
+        case 2: temp1=decr(num);
+                res=decr(temp1);
+                printf("the decremented number is %d\n",res);
+                num=res;
+                break;
+        case 3: exit(0);
+
+        default: printf("WRONG CHOICE ");
+        
     }
     }       
     
